5f78542965f84fbc82675a6d02ef87cd3565e946
Alex <alex@brewingcode.net>
2015-04-06 13:14:56 -0700

    commit script to monitor /r/thebutton

---
+++ bin/the_button.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+# adapted from http://zeflo.com/2015/the-reddit-button/
+
+import urllib2
+import re
+import websocket # pip install websocket-client, NOT websocket
+import json
+import sys
+
+req = urllib2.Request('http://www.reddit.com/r/thebutton', None)
+response = urllib2.urlopen(req)
+page = response.read()
+found = re.search('wss://(.+?)"', page)
+
+# if successfull
+if found:
+  print '\t'.join(['timer', 'min', 'participants'])
+
+  url = "wss://" + found.group(1)
+  ws = websocket.create_connection(url)
+  fewest = 60
+
+  while True:
+    result =  ws.recv()
+    result = json.loads(result)
+    seconds_left = result['payload']['seconds_left']
+    participants = result['payload']['participants_text']
+    if seconds_left < fewest:
+      fewest = seconds_left
+    print  '\t'.join(str(x) for x in [seconds_left,fewest,participants])
+
+  ws.close()
+
+else:
+  sys.stderr.write('no websocket url found at /r/thebutton')
