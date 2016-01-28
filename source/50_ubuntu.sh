63e78a0e4a144ea1ab702f5267ba1790406958d6
Alex <alex@brewingcode.net>
2016-01-28 15:33:03 -0800

    pare down source/50_ubuntu

--- source/50_ubuntu.sh
+++ source/50_ubuntu.sh
@@ -10,28 +10,3 @@ alias search="apt-cache search"
 # Make 'less' more.
 [ -x /usr/bin/lesspipe ] && eval "$(SHELL=/bin/sh lesspipe)"
 
-# Switch between already-downloaded node versions.
-function node_ver() {
-  (
-    ver="${1#v}"
-    nodes=()
-    if [[ ! -e "/usr/local/src/node-v$ver" ]]; then
-      shopt -s extglob
-      shopt -s nullglob
-      cd "/usr/local/src"
-      eval 'for n in node-v*+([0-9]).+([0-9]).+([0-9]); do nodes=("${nodes[@]}" "${n#node-}"); done'
-      [[ "$1" ]] && echo "Node.js version \"$1\" not found."
-      echo "Valid versions are: ${nodes[*]}"
-      [[ "$(type -P node)" ]] && echo "Current version is: $(node --version)"
-      exit 1
-    fi
-    cd "/usr/local/src/node-v$ver"
-    sudo make install >/dev/null 2>&1 &&
-    echo "Node.js $(node --version) installed." ||
-    echo "Error, $(node --version) installed."
-  )
-}
-
-eval $(perl -I${HOME}/perl5/lib/perl5 -Mlocal::lib)
-
-export LD_LIBRARY_PATH=/usr/lib:/usr/local/lib
