296e36f4891126b9ce3e033e713090c834a6e923
Alex <alex@brewingcode.net>
2016-01-29 16:09:16 -0800

    run sudo before rvm install, because autolibs requires sudo

--- init/30_rvm.sh
+++ init/30_rvm.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
 echo "https://rvm.io/rvm/install"
+
+echo "running sudo to verify you can do the root-level rvm install steps (autolibs)"
+sudo true
+[ $? -eq 0 ] || { echo "nope, sorry skipping rvm after all" >&2; exit 1; }
+
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
 curl -sSL https://get.rvm.io | bash -s stable --ruby
