728635d02ae2afa80afc22301085b3b5366958c5
Alex <alex@brewingcode.net>
2016-02-03 15:02:48 -0800

    fancy branch tip dump

--- source/50_vcs.sh
+++ source/50_vcs.sh
@@ -180,4 +180,8 @@ function gitsumm {
   done
 }
 
+function gitbr {
+  git for-each-ref --sort=committerdate refs/heads/ --format='%(HEAD) %(color:yellow)%(refname:short)%(color:reset) - %(color:red)%(objectname:short)%(color:reset) - %(contents:subject) - %(authorname) (%(color:green)%(committerdate:relative)%(color:reset))'
+}
+
 source $DOTFILES/vendor/git-completion.bash
