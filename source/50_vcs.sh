03e6ce3b7b104b9302110d68112741da1ac2cc9f
Alex <alex@brewingcode.net>
2016-03-07 15:09:00 -0800

    strip out aliases i have never used

--- source/50_vcs.sh
+++ source/50_vcs.sh
@@ -1,44 +1,13 @@
 
 # Git shortcuts
-
-alias g='git'
-function ga() { git add "${@:-.}"; } # Add all files by default
-alias gp='git push'
-alias gpa='gp --all'
-alias gu='git pull'
-alias gl='git log'
-alias gg='gl --decorate --oneline --graph --date-order --all'
-alias gs='git status'
-alias gst='gs'
-alias gd='git diff'
-alias gdc='gd --cached'
-alias gm='git commit -m'
-alias gma='git commit -am'
-alias gb='git branch'
-alias gba='git branch -a'
-function gc() { git checkout "${@:-master}"; } # Checkout master by default
-alias gco='gc'
-alias gcb='gc -b'
-alias gbc='gc -b' # Dyslexia
-alias gr='git remote'
-alias grv='gr -v'
-#alias gra='git remote add'
-alias grr='git remote rm'
-alias gcl='git clone'
 alias gcd='git rev-parse 2>/dev/null && cd "./$(git rev-parse --show-cdup)"'
 
 # Current branch or SHA if detached.
 alias gbs='git branch | perl -ne '"'"'/^\* (?:\(detached from (.*)\)|(.*))/ && print "$1$2"'"'"''
 
-# Run commands in each subdirectory.
-alias gu-all='eachdir git pull'
-alias gp-all='eachdir git push'
-alias gs-all='eachdir git status'
-
-alias gitup='git push --set-upstream origin "$(git rev-parse --abbrev-ref HEAD)"'
-
-alias gitrc='git rebase --continue'
-alias gitra='git rebase --abort'
+alias gup='git push --set-upstream origin "$(git rev-parse --abbrev-ref HEAD)"'
+alias grc='git rebase --continue'
+alias gra='git rebase --abort'
 
 gitre() { git rebase -i "$(git merge-base HEAD "${1:-master}")"; }
 gitfp() { git commit --fixup HEAD "$@"; gitre HEAD^^; }
@@ -75,31 +44,6 @@ gurl() {
 }
 # GitHub URL for current repo, including current branch + path.
 alias gurlp='echo $(gurl)/tree/$(gbs)/$(git rev-parse --show-prefix)'
-
-# git log with per-commit cmd-clickable GitHub URLs (iTerm)
-function gf() {
-  git log $* --name-status --color | awk "$(cat <<AWK
-    /^.*commit [0-9a-f]{40}/ {sha=substr(\$2,1,7)}
-    /^[MA]\t/ {printf "%s\t$(gurl)/blob/%s/%s\n", \$1, sha, \$2; next}
-    /.*/ {print \$0}
-AWK
-  )" | less -F
-}
-
-# open last commit in GitHub, in the browser.
-function gfu() {
-  local n="${@:-1}"
-  n=$((n-1))
-  git web--browse  $(git log -n 1 --skip=$n --pretty=oneline | awk "{printf \"$(gurl)/commit/%s\", substr(\$1,1,7)}")
-}
-# open current branch + path in GitHub, in the browser.
-alias gpu='git web--browse $(gurlp)'
-
-# Just the last few commits, please!
-for n in {1..5}; do alias gf$n="gf -n $n"; done
-
-function gj() { git-jump "${@:-next}"; }
-alias gj-='gj prev'
 
 # OSX-specific Git shortcuts
 if is_osx; then
