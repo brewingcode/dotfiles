7c7f2020b12b8f8f3047efdfbe0644ecaf58c5fb
Alex <alex@brewingcode.net>
2016-10-15 14:22:03 -0700

    remove "dotfiles" from bin dir, it will be pared down to just util functions

--- init.sh
+++ init.sh
@@ -1,4 +1,6 @@
-[ -d "$DOTFILES" ] || echo { \$DOTFILES does not point to a directory >&2; return 1; }
+# bootstrap for dotfiles: source this file
+DOTFILES=$(pwd)/$(dirname "$_")
+[ -d "$DOTFILES" ] || { echo "\$DOTFILES=$DOTFILES does not point to a directory" >&2; return 1; }
 
 # OS detection: some things are only OSX, others are only Ubuntu, but
 # both are in the minority
@@ -10,26 +12,29 @@ is_ubuntu() {
 }
 
 [[ is_osx ]] && source "$DOTFILES/osx.sh"
-[[ is_ubuntu ]] & source "$DOTFILES/ubuntu.sh"
+[[ is_ubuntu ]] && source "$DOTFILES/ubuntu.sh"
 
 # add binaries into the path
 export PATH="$DOTFILES/bin:$PATH"
 
+# make splat smarter
+shopt -s dotglob nullglob
+
 # source all the things
-cd "$DOTFILES"/source
+cd "$DOTFILES/source" || { echo "$DOTFILES/source is not a directory" >&2; return 2; }
 for f in *; do
   source "$f"
 done
 
 # make sure links exist
-cd "$DOTFILES"/links
+cd "$DOTFILES/link" || { echo "$DOTFILES/link is not a directory" >&2; return 3; }
 for f in *; do
-  ln -fs "$DOTFILES/links/$f" "$HOME/$f"
+  ln -fs "$DOTFILES/link/$f" "$HOME/$f"
 done
 
 # make sure files have been copied, but only if they don't exists already
-cd "$DOTFILES/copy"
+cd "$DOTFILES/copy" || { echo "$DOTFILES/copy is not a directory" >&2; return 4; }
 for f in *; do
-  [ -e "$HOME/$f" ] || cp -r "$f" "$HOME"
+  [ -e "$HOME/$f" ] || cp -r "$f" "$HOME/$f"
 done
 
